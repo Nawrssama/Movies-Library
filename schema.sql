5707c87147a8ea4af5e1bacda5ce1a8664396121
Nawrssama <nawrsbibi.nb@gmail.com>
2023-02-28 08:03:09 +0000

    created a schema.sql

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS mymovies; 
+
+CREATE TABLE IF NOT EXISTS mymovies (
+    id SERIAL PRIMARY KEY,
+    movie_name VARCHAR(255),
+    movie_time VARCHAR(255),
+    releas_date DATETIME(255),
+    overview VARCHAR(1000),
+    main_language VARCHAR(255),
+    recomanded_age INT(255)
+);
